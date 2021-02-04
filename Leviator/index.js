459f23f49894fc0df64fd5bb6280e70a2ef90893
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-02-04 22:54:54 +0100

    🚧 Index.js

---
+++ Leviator/index.js
@@ -0,0 +1,37 @@
+const { Client, Collection } = require('discord.js');
+const { TOKEN } = require("./config.json");
+const { readdirSync } = require('fs');
+const close = require('./commands/misc/close.js');
+
+const client = new Client();
+client.login(TOKEN);
+
+client.commands = new Collection();
+
+const loadCommands = (dir = "./commands/") => {
+    readdirSync(dir).forEach(dirs => {
+        const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith('.js'));
+
+        for (const file of commands) {
+            const getFileName = require(`${dir}/${dirs}/${file}`);
+            client.commands.set(getFileName.help.name, getFileName);
+            console.log(`${getFileName.help.name} command loaded`)
+        }
+    });
+}
+
+const loadEvents = (dir = "./events/") => {
+    readdirSync(dir).forEach(dirs => {
+        const events = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith('.js'));
+
+        for (const event of events) {
+            const evt = require(`${dir}/${dirs}/${event}`);
+            const evtName = event.split('.')[0];
+            client.on(evtName, evt.bind(null, client));
+            console.log(`${evtName} event loaded`);
+        }
+    });
+}
+
+loadCommands();
+loadEvents();
