b5c8ad89ddc9e2b06d36dee2298b843a660e80b1
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-02-04 21:33:17 +0100

    🚧  post user

---
+++ API/post/user.js
@@ -0,0 +1,33 @@
+const sqlite3 = require('sqlite3').verbose();
+
+module.exports.run = (app) => {
+    app.post('/user', async (req, res) => {
+
+      const id = req.body.id
+      const username = req.body.username
+      const xp = req.body.xp
+
+      if (!id || !username) {
+        res.send('Args missing !')
+        return
+      }
+
+      let sql = `INSERT INTO user (id,xp,username) VALUES (${id},${xp},'${username}')`
+
+      let db = new sqlite3.Database('./database/db.db', (err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+      });
+
+      db.run(sql, [], function(err) {
+        if (err) {
+          res.send('Error: ' + err.message)
+          return console.error(err.message);
+        }
+        console.log(`New user inserted id:${id}`);
+        res.send(`New user inserted id:${id}`);
+      });
+
+    });
+}
