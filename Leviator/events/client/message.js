875e420014fcb4dd00484f1930354c5429e585ea
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-02-11 20:51:02 +0100

    🚧 Add message event

---
+++ Leviator/events/client/message.js
@@ -0,0 +1,17 @@
+const { PREFIX } = require("../../config.json");
+
+module.exports = (client, message) => {
+
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.help.aliases && cmd.help.aliases.includes(commandName));
+    if (!command) return;
+
+    command.run(client, message, args);
+}
+
