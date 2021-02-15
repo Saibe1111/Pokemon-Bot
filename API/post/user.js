61dcdb1122517653bb07171fadbfb6ff9fbc6f0c
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-02-15 19:24:01 +0100

    🚧 Post user

--- API/post/user.js
+++ API/post/user.js
@@ -5,6 +5,9 @@ module.exports.run = (app) => {
 
       const id = req.query.id
       const username = req.query.username
+      let xp = req.query.xp
+      let addXp = req.query.addXp
+      let removeXp = req.query.removeXp
 
       if (!id || !username) {
         res.send('Args missing !')
@@ -18,7 +21,7 @@ module.exports.run = (app) => {
       });
 
 
-      let sql = `SELECT ID id FROM User WHERE ID= ?`;
+      let sql = `SELECT ID id, XP xp FROM User WHERE ID= ?`;
 
       db.get(sql, [id], (err, row) => {
         if (err) {
@@ -26,23 +29,54 @@ module.exports.run = (app) => {
         }
 
         if(row){
-          res.send(`L'id "${row.id}" existe déjà !`)
+          updateUser(res, db, row, xp, removeXp, addXp, id, username);
         }else{
-          console.log('Y a rien ! Je m en occupe')
-          
-          let sql = `INSERT INTO user (id,xp,username) VALUES (${id},0,'${username}')`
-
-          db.run(sql, [], function(err) {
-            if (err) {
-              res.send('Error: ' + err.message)
-              return console.error(err.message);
-            }
-            console.log(`New user inserted id: ${id}`);
-            res.send(`New user inserted id: ${id}`);
-          });
+          addUser(res, db, id, username);
         }
 
       });
 
+    });
+}
+
+function addUser(res, db, id, username){
+  console.log('Y a rien ! Je m en occupe')
+  let sql = `INSERT INTO user (ID,XP,Username) VALUES (${id},0,'${username}')`
+
+  db.run(sql, [], function(err) {
+    if (err) {
+      res.send('Error: ' + err.message)
+      return console.error(err.message);
+    }
+    console.log(`New user inserted id: ${id}`);
+    res.send(`New user inserted id: ${id}`);
+  });
+}
+
+function updateUser(res, db, row, xp, removeXp, addXp, id, username){
+
+      if(!xp && !removeXp){
+        xp = row.xp + parseInt(addXp);
+      }
+      if(!xp && !addXp){
+        xp = row.xp - parseInt(removeXp);
+      }
+      if(!addXp && !removeXp){
+        xp = xp;
+      }
+      if(!addXp && !removeXp && !xp){
+        xp = row.xp + 1;
+      }
+
+
+    let sql = `UPDATE user SET XP= ?, Username= ? WHERE ID= ?`
+
+    db.run(sql, [xp, username, id], function(err) {
+      if (err) {
+        res.send('Error: ' + err.message)
+        return console.error(err.message);
+      }
+      console.log(`User update id: ${id}`);
+      res.send(`User update id: ${id}`);
     });
 }
